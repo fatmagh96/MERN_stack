869590836f76cdf2bf6c53765ad005cb9f9ed125
fatmagh96 <98395704+fatmagh96@users.noreply.github.com>
2023-09-01 15:35:57 +0100

    big inversion

---
+++ day_05_react_functional/Core/big_inversion/src/components/PersonCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PersonCard(props) {
+  return (
+    <>
+      
+      <h1>{props.person.lastName}, {props.person.firstName}</h1>
+      <p>Age: {props.person.age}</p>
+      <p>Hair Color: {props.person.hair_color}</p>
+
+    </>
+  )
+}
+
+export default PersonCard
